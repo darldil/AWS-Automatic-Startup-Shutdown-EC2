879aed4a7bef78e75d9739c24fdc8e2b59c37904
Darldil <darldil@users.noreply.github.com>
2018-05-03 23:28:44 +0200

    Create ShutdownScript.py

---
+++ ShutdownScript.py
@@ -0,0 +1,27 @@
+import boto3
+
+def lambda_handler(event, context):
+
+    if not bool(event):
+        raise Exception('No se ha definido ningún evento.')
+        
+    instances = event.get("instances")
+    region = event.get("region")
+        
+    if not bool(instances):
+        raise Exception('Error: No se han proporcionado instancias')
+        
+    if not type(instances) is list:
+        raise Exception('Error: Las instancias no están en la lista')
+        
+    if not bool(instances):
+        raise Exception('Error: No se han proporcionado instancias')
+
+    if not bool(region):
+        raise Exception('Error: No se ha proporcionado una region')
+
+    ec2 = boto3.client('ec2', region_name=region)
+    ec2.stop_instances(InstanceIds=instances)
+        
+    for instance in instances:
+        print "Detenida instancia {} en la region {}".format(instance, region)
